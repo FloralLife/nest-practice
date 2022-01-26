288680ae747e8a990a2f539130d923c071ee5b34
FloralLife <ocard9611@naver.com>
2022-01-26 17:07:35 +0900

    feat: typeorm, 환경 변수 사용하도록 추가

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig = (): TypeOrmModuleOptions => {
+  return {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: true
+  };
+};
