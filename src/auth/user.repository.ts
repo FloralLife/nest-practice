b89e041542133dcebe767df3146c47fecd2fcebb
FloralLife <ocard9611@naver.com>
2022-01-28 02:17:51 +0900

    feat: 유저 이름 고유한 값으로 저장되도록 변경
    - 고유한 값으로 저장되지 않았을 때 존재하는 이름이라는 메시지 전달

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
@@ -7,7 +8,13 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<User> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (err) {
+      if (err.code === '23505') throw new ConflictException('Existing username');
+      throw new InternalServerErrorException();
+    }
     return user;
   }
 }
