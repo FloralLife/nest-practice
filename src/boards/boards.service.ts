e0e39e4e5f6f85ff9bc651da168307fa0884e7c3
FloralLife <ocard9611@naver.com>
2022-01-30 20:52:57 +0900

    feat: 게시글 추가시 jwt에 있는 유저 정보도 추가되도록 변경

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/user.entity';
 import { v1 as uuid } from 'uuid';
 import { BoardStatus } from './board-status.enum';
 import { Board } from './board.entity';
@@ -10,8 +11,8 @@ import { CreateBoardDto } from './dto/create-board.dto';
 export class BoardsService {
   constructor(@InjectRepository(BoardRepository) private boardRepository: BoardRepository) {}
 
-  createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
-    return this.boardRepository.createBoard(createBoardDto);
+  createBoard(createBoardDto: CreateBoardDto, user: User): Promise<Board> {
+    return this.boardRepository.createBoard(createBoardDto, user);
   }
 
   async getAllBoards(): Promise<Board[]> {
